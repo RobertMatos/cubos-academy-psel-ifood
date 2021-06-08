7fbc05c14c857176ef84365540f0de035c4ce0a8
Robert de Matos <robertvmatos@gmail.com>
2021-06-08 16:36:18 -0300

    Resolução do quarto desafio do PSEL/ifood 2021

---
+++ contagem_regresiva.js
@@ -0,0 +1,10 @@
+function solucao(numero) {
+	//seu codigo aqui
+    while(numero!=-1){                
+        console.log(numero)
+        numero--
+    }
+    if(numero == -1){
+        console.log('KABUM')
+    }
+}
